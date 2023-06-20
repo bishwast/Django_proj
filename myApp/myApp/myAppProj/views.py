6913c2c3cc40180e5c1e8250bf0f148e4401284a
Sunil T Bishwakarma <bishwast77@gmail.com>
2023-06-20 14:07:38 -0600

    GET vs POST in Django
    -	GET Method: User input will be shown in the url. This is set to fefault.
    -	POST Method: User inputs will not be shown in the url
    -	CSRF: Cross Site Request Forgery
    -	Django provides a default CSRF Token that protects data from CSRF attacks.
    -	In POST if CSRF Token is not used in Django the form will not work.
    -	Changed the request method to POST in index.html and views.py counter func.

--- myApp/myApp/myAppProj/views.py
+++ myApp/myApp/myAppProj/views.py
@@ -6,6 +6,6 @@ def index(request):
     return render(request, 'index.html')  
 
 def counter(request):
-    text = request.GET['text']  ## stores user input from index.html to text variable.
+    text = request.POST['text']  ## stores user input from index.html to text variable.
     amountofWords = len(text.split())   ## Splits each unspaced combination of letters to words and counts them.
     return render(request, 'counter.html', {'amount': amountofWords})   # custom key "amount" that stores the count of all words.
