4a00f998edc5103aca18d0f1be0ac73e7d75ce74
Sunil T Bishwakarma <bishwast77@gmail.com>
2023-06-20 21:17:46 -0600

    Models in Django, passing static data from backend to frontend.
    -	Modules in Django are used to configure a database.
    -	From Model we pass data into the template folder.
    -	Using classes in Python to build the database.
    -	Added a class in models.py file
    -	Views.py > Imported models. Initialized Feature() class in the index function.
    -	In index() func added feature attributes to the attributes in the class inherited.
    -	Then added feature to be rendered.
    -	In index.html, changed one of the feature to the name attribute in Feature class as {{feature.name}}, changed details to {{feature.details}}
    -	Repeated same for other features.
    -	Now the data in webpage is coming from the backend.
    -	So far the code is static as whatever I am passing from the views.py is being rendered in the front-end.

--- myApp/myApp/myAppProj/views.py
+++ myApp/myApp/myAppProj/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')  
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is awesome'
+    
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.details = 'You can rely on our experts'
+    
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Easy to use'
+    feature3.details = 'You will find our services very easy to follow and use'
+    
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Affordable'
+    feature4.details = 'We have affordable prices for our services'
+    
+    return render(request, 'index.html', {'feature1': feature1, 
+                                          'feature2': feature2, 
+                                          'feature3': feature3, 
+                                          'feature4': feature4})  
 
 def counter(request):
     text = request.POST['text']  ## stores user input from index.html to text variable.
