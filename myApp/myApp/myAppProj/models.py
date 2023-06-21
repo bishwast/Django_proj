90ca0ae5db0534e49e963100c9ae3faafee79b1e
Sunil T Bishwakarma <bishwast77@gmail.com>
2023-06-20 21:47:44 -0600

    Dynamic data rendering with if-else
    -	Added is_True features for Boolean datatype and applied conditions on each feature attribute using if-else in html.

--- myApp/myApp/myAppProj/models.py
+++ myApp/myApp/myAppProj/models.py
@@ -5,4 +5,4 @@ class Feature:
     id: int
     name: str
     details: str
-    
+    is_true: bool       # For each features check if they are True or Not
--- myApp/myApp/myAppProj/views.py
+++ myApp/myApp/myAppProj/views.py
@@ -7,21 +7,25 @@ def index(request):
     feature1 = Feature()
     feature1.id = 0
     feature1.name = 'Fast'
+    feature1.is_true = True
     feature1.details = 'Our service is awesome'
     
     feature2 = Feature()
     feature2.id = 1
     feature2.name = 'Reliable'
+    feature2.is_true = True
     feature2.details = 'You can rely on our experts'
     
     feature3 = Feature()
     feature3.id = 2
     feature3.name = 'Easy to use'
+    feature3.is_true = False
     feature3.details = 'You will find our services very easy to follow and use'
     
     feature4 = Feature()
     feature4.id = 3
     feature4.name = 'Affordable'
+    feature4.is_true = True
     feature4.details = 'We have affordable prices for our services'
     
     features = [feature1, feature2, feature3, feature4]
