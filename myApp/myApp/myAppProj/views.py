dc7857d139352f6e0ecd3fd0782ac355dd163116
Sunil T Bishwakarma <bishwast77@gmail.com>
2023-06-22 22:27:58 -0600

    Dynamic URLs
    -	Added path to URL patterns; path(‘post/<str:pk>’) of a name ‘pk’
    -	Added new func in views.py to collect the data stored in post pk variable
    -	Pk variable is what stores each account in this application, which calls the profile value in a dynamic way.

--- myApp/myApp/myAppProj/views.py
+++ myApp/myApp/myAppProj/views.py
@@ -97,3 +97,6 @@ def counter(request):
     text = request.POST['text']  ## stores user input from index.html to text variable.
     amountofWords = len(text.split())   ## Splits each unspaced combination of letters to words and counts them.
     return render(request, 'counter.html', {'amount': amountofWords})   # custom key "amount" that stores the count of all words.
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
